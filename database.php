971739d257126f6d98df847f4a444d575cec24e0
zhihutop <75374221+zhihutop@users.noreply.github.com>
2020-12-02 21:28:37 +0800

    互联网时代的好
    线上学习的便捷，让自己更广泛的吸收精神食粮！找到学习的方法坚持去学去实践很重要！

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+if (! defined('BASEPATH')) exit('No direct script access allowed');
+include_once dirname(__FILE__).'/../../data/config.php';
+
+$active_group = 'default';
+$query_builder = TRUE;
+$db = $db;
